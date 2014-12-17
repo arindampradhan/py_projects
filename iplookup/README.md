d11bf80c47c881d789311afb59031b7c60634d66
arindampradhan <arindampradhan10@gmail.com>
2014-12-17 17:15:11 +0530

    ip lookup

--- iplookup/README.md
+++ iplookup/README.md
@@ -23,3 +23,22 @@ Simply run:
 	lat : 37.7898
 	org : Wikimedia Foundation
 	regionName : California
+	
+	$ python iplookup.py json
+	Enter your ip address: 208.80.152.201
+	{
+	    "status": "success", 
+	    "city": "San Francisco", 
+	    "zip": "94105", 
+	    "countryCode": "US", 
+	    "country": "United States", 
+	    "region": "CA", 
+	    "isp": "Wikimedia Foundation", 
+	    "lon": -122.3942, 
+	    "timezone": "America/Los_Angeles", 
+	    "as": "AS14907 Wikimedia Foundation Inc.", 
+	    "query": "208.80.152.201", 
+	    "lat": 37.7898, 
+	    "org": "Wikimedia Foundation", 
+	    "regionName": "California"
+}
--- iplookup/iplookup.py
+++ iplookup/iplookup.py
@@ -1,15 +1,24 @@
-# using http://ip-api.com/docs/api:json
-# thanks for the api
-# http://ip-api.com/json/208.80.152.201?callback=yourfunction
+# Using http://ip-api.com/docs/api:json
+# Thanks for the free Api
+# USAGE
+# =====
+# $ python iplookup.py 
+# $ python iplookup.py json
 
 import json
 import urllib2
+import sys
 
 ip = raw_input("Enter your ip address: ")
 
-req = urllib2.urlopen("http://ip-api.com/json/"+str(ip)).read()
+req = urllib2.urlopen("http://ip-api.com/json/"+ip).read()
 
 json_req = json.loads(req)
 
-for key, value in json_req.items() :
-    print "{0} : {1}".format(key, value)
+try:
+	if sys.argv[1] == "json":
+		print json.dumps(json_req,indent=4)
+except: 
+	for key, value in json_req.items() :
+	    print "{0} : {1}".format(key, value)
+
