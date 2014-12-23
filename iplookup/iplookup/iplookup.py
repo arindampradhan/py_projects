baa81dab8b29936c98a6ecedb9fa552a7bb61162
arindampradhan <arindampradhan10@gmail.com>
2014-12-24 01:11:33 +0530

    added docopt parser

--- iplookup/iplookup/iplookup.py
+++ iplookup/iplookup/iplookup.py
@@ -3,79 +3,71 @@
 
 # Using http://ip-api.com/docs/api:json
 # Thanks for the free Api
+# Whois API charge a lot   t ( - _ - t )
 # USAGE
 # =====
 # $ iplookup <ip>
 
-import json
+""" iplookup
+    
+    Usage:
+        iplookup -h,--help
+        iplookup <ip> [ -j | -s | -z | -c | -y | -r | -i | -l | -g | -t | -q | -o | -n ]
+        iplookup <repeating> ...
+
+    Options:
+        -h,--help       : show this help message
+        ip        : example of a ip argument
+        repeating       : example of repeating arguments
+        -j,--json       : show the json file of the lookup
+        -s,--status     : show the status,if available or not
+        -z,--zip        : show the zip code the the ip
+        -c,--country    : show the country where the ip is located
+        -y,--city       : show the location where the ip is located
+        -r,--region     : show the region of the ip
+        -i,--isp        : show the organization or the foundation
+        -l,--lat        : show the latitude of the location
+        -t,--timezone   : show the timezone 
+        -q,--query      : show the query you made
+        -o,--org        : show the organization
+        -n,--lon        : show the longitude
+        -g,--regionName   : show the timezone 
+"""
+from docopt import docopt
+from docopt import sys
 import urllib2
-import sys
+import json
 
-def parser(json_req,mode):
-	"""
-	$ iplookup <parameter> | <ip>
-
-	Not getting any ip dude ¯\_(ツ)_/¯
-
-	eg. google ip: 64.233.160.0
-	$ iplookup 64.233.160.0
-
-	Optional arguments: <parameter>
-	-k, -json 			show the json file of the lookup
-	-s, -status			show the status,if available or not
-	-c, -city			show the location where the ip is located
-	-z, -zip			show the zip code the the ip
-	-c, -countryCode		show the countrycode of the ip
-	-c, -country 			show the country where the ip is located
-	-r, -region 			show the region of the ip
-	-i, -isp			show the organization or the foundation
-	-l, -lon 			show the longitude
-	-t, -timezone			show the timezone
-	-a, -as 			show the Incorporation
-	-q, -query			show the query you made
-	-l, -lat 			show the latitude of the location
-	-o, -org 			show the organization
-	-r, -regionName 		show the regionName
-	"""
-	# really not pythonic :( I know, but argparse sucks!!
-
-	if mode[0] == "-":
-		mode = mode[1:]
-		if mode.lower() == "j":
-			return json.dumps(json_req)
-		if mode.lower() == "json":
-			return json.dumps(json_req,indent=4)
-		if mode in ("status","s"):
-			return json_req["status"]
-    	if mode in ("city","c"):
-			return json_req["city"]
-    	if mode in ("zip","z"):
-			return json_req["zip"]
-    	if mode in ("countryCode","cc"):
-			return json_req["countryCode"]
-    	if mode in ("country","c"):
-			return json_req["country"]
-    	if mode in ("region","r"):
-			return json_req["region"]
-    	if mode in ("isp","i"):
-			return json_req["isp"]
-    	if mode in ("lon","l"):
-			return json_req["lon"]
-    	if mode in ("timezone","t"):
-			return json_req["timezone"]
-    	if mode in ("as","a"):
-			return json_req["as"]
-    	if mode in ("query","q"):
-			return json_req["query"]
-    	if mode in ("lat","l"):
-			return json_req["lat"]
-    	if mode in ("org","o"):
-			return json_req["org"]
-    	if mode in ("regionName","rn","rN"):
-    		return json_req["regionName"]
-	else:
-		print "No such parameter available dude ｍ（．＿．）ｍ"
-		sys.exit()
+def parser(json_req,args):
+	if args['--json']:
+		print json.dumps(json_req,indent=4)
+	if args['--status']:
+		print json_req['status']
+	if args['--zip']:
+		print json_req['zip']
+	if args['--country']:
+		print json_req['country']
+	if args['--region']:
+		print json_req['region']
+	if args['--isp']:
+		print json_req['isp']
+	if args['--lon']:
+		print "lon: ",json_req['lon']
+	if args['--lat']:
+		print json_req['lat']
+	if args['--timezone']:
+		print json_req['timezone']
+	if args['--query']:
+		print json_req['query']
+	if args['--org']:
+		print json_req['org']
+	if args['--lon']:
+		print json_req['lon']
+	if args['--regionName']:
+		print json_req['regionName']
+	if args['<ip>'] and (len(sys.argv) == 2):
+		for key, value in json_req.items() :
+		    print "{0} : {1}".format(key, value)
 
 def _build_json(ip):
 	req = urllib2.urlopen("http://ip-api.com/json/{}".format(ip)).read()
@@ -83,16 +75,13 @@ def _build_json(ip):
 	return json_req
 
 def main():
-	if len(sys.argv) < 2:
-		print parser.__doc__
-		sys.exit()
-	if len(sys.argv) == 2:
-		json_req = _build_json(sys.argv[1])
-		for key, value in json_req.items() :
-		    print "{0} : {1}".format(key, value)
-	if len(sys.argv) == 3:
-		json_req = _build_json(sys.argv[2])
-		print parser(json_req,sys.argv[1])
+	args = docopt(__doc__,version='iplookup 0.2.0')
+	if args['<ip>']:
+		json_req = _build_json(args['<ip>'])
+		parser(json_req,args)
+	else:
+		print "Not getting any ip dude ¯\_(ツ)_/¯"
+		exit
 
 if __name__ == "__main__":
 	main()
