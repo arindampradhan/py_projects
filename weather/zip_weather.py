c6fe6a4898245036c1ba6d74373d1d4e7c1cfc3d
Arindam Pradhan <arindampradhan10@gmail.com>
2015-01-02 00:03:52 +0530

    update weather zip code.

--- weather/zip_weather.py
+++ weather/zip_weather.py
@@ -13,7 +13,7 @@ def weather(city):
 # http://stackoverflow.com/questions/2846466/how-to-convert-postal-code-to-city-name-is-there-an-api-available
 # get the city from postal code using Google maps Geocoding API
 def get_city(zip_code):
-	city_info = requests.get("http://maps.googleapis.com/maps/api/geocode/json?address=94043&sensor=false")
+	city_info = requests.get("http://maps.googleapis.com/maps/api/geocode/json?address={}&sensor=false".format(zip_code))
 	city_name = city_info.json()['results'][0]['address_components'][1]['long_name']
 	return city_name
 
